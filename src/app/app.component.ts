1b5c3997919746389bfee4ed2e93547c4bc87947
bfeldbacher <b.feldbacher@abios.at>
2022-05-27 10:17:58 +0200

    Add gradient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,16 @@ export class AppComponent {
     [252, 218, 92], // 3 = yellow
     [50, 187, 22], // 4 = green
   ];
-  gradientWidth = 20;
+  gradientWidth = 10;
 
   getColor(pulse: number, gradient = false): string {
-    let hex = gradient ? this.getColorAsHexArrayWithGradient(pulse) : this.getColorAsHexArray(pulse);
+    let hex = gradient
+      ? this.getColorAsHexArrayWithGradient(pulse)
+      : this.getColorAsHexArray(pulse);
     return 'rgb(' + hex.join(',') + ')';
   }
 
   getColorAsHexArray(pulse: number): number[] {
-    let halfGradientWidth = this.gradientWidth / 2;
     // anything below lowest belong to first zone. no gradient
     if (pulse < this.zones[0]) {
       return this.colorIdToColor(0);
@@ -30,7 +31,6 @@ export class AppComponent {
 
     // iterate through zones
     for (let i = 0; i < this.zones.length - 1; i++) {
-      // todo handle zones smaller than gradient width
       if (this.zones[i] <= pulse && pulse < this.zones[i + 1]) {
         return this.colorIdToColor(i);
       }
@@ -48,6 +48,34 @@ export class AppComponent {
 
     // iterate through zones
     for (let i = 0; i < this.zones.length - 1; i++) {
+      // gradient at end of zone
+      if (
+        i + 2 < this.zones.length &&
+        this.zones[i + 1] - halfGradientWidth <= pulse &&
+        pulse < this.zones[i + 1]
+      ) {
+        let weight = (this.zones[i + 1] - pulse) / halfGradientWidth / 2 + 0.5;
+        return this.pickHex(
+          this.colorIdToColor(i),
+          this.colorIdToColor(i + 1),
+          weight
+        );
+      }
+
+      // gradient at start of zone
+      if (
+        i - 1 >= 0 &&
+        this.zones[i] <= pulse &&
+        pulse < this.zones[i] + halfGradientWidth
+      ) {
+        let weight = (1 - (pulse - this.zones[i]) / halfGradientWidth) / 2;
+        return this.pickHex(
+          this.colorIdToColor(i - 1),
+          this.colorIdToColor(i),
+          weight
+        );
+      }
+
       // todo handle zones smaller than gradient width
       if (this.zones[i] <= pulse && pulse < this.zones[i + 1]) {
         return this.colorIdToColor(i);
@@ -56,7 +84,6 @@ export class AppComponent {
 
     return this.colorIdToColor(this.colorIds.length - 1);
   }
-
 
   colorIdToColor(index: number): number[] {
     return this.colors[this.colorIds[index] - 1];
